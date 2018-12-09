211a63b5243111a66628aeb76a208d54acb0b258
Trine <trinem95@hotmail.com>
2018-12-09 22:06:50 +0100

    oppdatering av forside

---
+++ FORSIDE/main.js
@@ -0,0 +1,53 @@
+
+var figure = $(".box").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).load(); 
+}
+
+// var stopHover = document.getElementsByTagName("video"); 
+
+// if ($(stopHover).width() < 780) {
+//     style.display = "none";  
+//  }
+//  else {
+//     style.display = "block"; 
+//  }
+
+
+
+var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
+
+showSlides();
+
+function showSlides() {    
+  var i;    
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex> slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function currentSlide(no) {
+  var i;    
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slideIndex = no;
+  slides[no-1].style.display = "block";
+}
+
+function plusSlides(n) {
+var newslideIndex = slideIndex + n;
+if(newslideIndex < 5 && newslideIndex > 0){
+   currentSlide(newslideIndex);
+}
+}
---
+++ FORSIDE/readme.md
@@ -0,0 +1,5 @@
+# Temaoppgave 9 - Informasjonsarkitektur 
+
+##### I forbindelse med skoleoppgave. I dette temaet skulle vi lage et eget web magasin med valgfritt tema. 
+
+##### Laget august - september 2018
